75e450928485adc405a4ab7da817fe28575367b7
BEN-ESSAHRAOUI-Yassine <ybenessahraoui@gmail.com>
2026-06-23 16:59:33 +0100

    Add queue

--- app/Models/RawContent.php
+++ app/Models/RawContent.php
@@ -6,6 +6,7 @@ use App\Enums\RawContentStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class RawContent extends Model
 {
@@ -34,5 +35,10 @@ class RawContent extends Model
     public function blueprint(): BelongsTo
     {
         return $this->belongsTo(Blueprint::class);
+    }
+
+    public function generatedPost(): HasOne
+    {
+        return $this->hasOne(GeneratedPost::class);
     }
 }
